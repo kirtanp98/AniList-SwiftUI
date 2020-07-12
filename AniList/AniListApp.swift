1372ba7dc03a9902a3e3c995ba1ed93c389d83d1
Kirtan Patel <kirtanpatl@gmail.com>
2020-07-12 14:43:12 -0400

    Initial Commit

---
+++ AniList/AniListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AniListApp.swift
+//  AniList
+//
+//  Created by Kirtan Patel on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AniListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
