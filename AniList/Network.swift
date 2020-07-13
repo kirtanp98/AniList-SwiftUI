91af35b9ea7ecc27dcc9b8a0ff5d842a78aeec33
Kirtan Patel <kirtanpatl@gmail.com>
2020-07-13 19:29:26 -0400

    Add Network singleton

---
+++ AniList/Network.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  AniList
+//
+//  Created by Kirtan Patel on 7/13/20.
+//
+import Apollo
+import CoreData
+import Foundation
+
+class Network {
+  static let shared = Network()
+    
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://graphql.anilist.co ")!)
+}
