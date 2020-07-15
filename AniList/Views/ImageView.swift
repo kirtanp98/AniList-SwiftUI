68e0b544c73c4296edb378fe7397bf90e8965440
Kirtan Patel <kirtanpatl@gmail.com>
2020-07-14 21:26:03 -0400

    Load GraphQL query and image to the view
    Code still needs some refactoring to make it clean

---
+++ AniList/Views/ImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageView.swift
+//  AniList
+//
+//  Created by Kirtan Patel on 7/14/20.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
